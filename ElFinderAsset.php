48e2b7248a59a9f235631652d7edb00ea0e5cd62
Samuel Onyijne <samuelord1@gmail.com>
2017-05-03 12:53:41 +0100

    Update ElFinderAsset.php

--- ElFinderAsset.php
+++ ElFinderAsset.php
@@ -27,9 +27,20 @@ class ElFinderAsset extends AssetBundle
     public function init()
     {
         parent::init();
+        if (class_exists('yii\jui\JuiAsset')) {
+              $this->depends[] = 'yii\jui\JuiAsset';
+        } elseif (class_exists('yii\web\JqueryAsset')){
+            $this->depends[] = 'yii\web\JqueryAsset';
+            $this->js[] = '//code.jquery.com/ui/1.12.1/jquery-ui.js';
+        } else {
+            $this->js[] = '//code.jquery.com/jquery-1.12.4.js';
+            $this->js[] = '//code.jquery.com/ui/1.12.1/jquery-ui.js';
+        }
         $this->js[] = YII_DEBUG ? 'js/elfinder.full.js' : 'js/elfinder.min.js';
         $this->css[] =  YII_DEBUG ? 'css/elfinder.min.css' : 'css/elfinder.full.css';
+        $this->css[] = '//code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css';
         $this->css[] = 'css/theme.css';
     }
+   
    
 }
